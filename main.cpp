a19bb12111ff91741d818f10f2acc7916e3c97fc
Andrew <lindstromandrew@gmail.com>
2015-03-10 14:47:00 -0400

    added main.cpp and functionality for basic window

---
+++ main.cpp
@@ -0,0 +1,53 @@
+#include "framework.h"
+#include <fstream>
+
+int WINAPI WinMain(HINSTANCE hInstance, 
+						 HINSTANCE hPrevInstance,
+						 LPSTR lpCmdline,
+						 int showCmd
+						)
+{
+	Framework* framework_Inst;
+	bool result;
+	std::ofstream fout;
+	
+	fout.open("error.log");
+	
+	framework_Inst = new Framework;
+	if(!framework_Inst){
+		fout << "Framework Not Created /n";
+		return 0;
+	}
+	
+	result = framework_Inst->Initialize();
+	if(result)
+	{
+		fout << "It Worked!";
+	}
+	else
+	{
+		fout << "Framework Not Initialized /n";
+	}
+	
+	framework_Inst->Shutdown();
+	delete framework_Inst;
+	framework_Inst = 0;
+	
+	fout.close();
+	
+	return 0;	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+

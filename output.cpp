f9db71e7b7cf1a259d569906ecba76cabf0ced55
Andrew <lindstromandrew@gmail.com>
2015-03-16 15:12:03 -0400

    Started work on creating output

--- output.cpp
+++ output.cpp
@@ -1,1 +1,51 @@
 #include "output.h"
+
+output::output()
+{
+	m_openGL = 0;
+}
+
+output::output(const output& other)
+{
+
+}
+
+output::~output()
+{
+
+}
+
+bool output::Initialize(OGL* ogl, HWND hwnd)
+{
+	m_openGL = ogl;
+
+	return true;
+}
+
+bool output::Frame()
+{
+	bool result;
+
+	result = Render();
+	if(!result)
+	{
+		return false;
+	}
+
+	return true;
+}
+
+void output::Shutdown()
+{
+	m_openGL = 0;
+}
+
+bool output::Render()
+{
+	m_openGL->BeginScene(0.5f,0.5f,0.5f,1.0f);
+
+
+	m_openGL->EndScene();
+
+	return true;
+}
--- output.h
+++ output.h
@@ -3,14 +3,25 @@
 
 #include "ogl.h"
 
+const float SCREEN_DEPTH = 1000.0f;
+const float SCREEN_NEAR = 0.1f;
+const bool VSYNC_ENABLED = false;
+
 class output
 {
 	public:
-	
+		output();
+		output(const output&);
+		~output();
+
+		bool Initialize(OGL* ogl, HWND hwnd);
+		bool Frame();
+		void Shutdown();
+
 	private:
-	
-	
-	
+		bool Render();
+
+		OGL* m_openGL;
 };
 
 #endif
